e0cf4cbe138c8d2f2921ffe53c9f4ae6df1d366d
P-Attila <94472248+P-Attila@users.noreply.github.com>
2022-04-28 07:09:51 +0200

    fix3

--- src/App.js
+++ src/App.js
@@ -1,62 +1,63 @@
-import { useState, useEffect } from "react";
-import MovieCard from "./MovieCard";
-import "./App.css";
-import SearchIcon from "./search.svg";
+import React, { useState, useEffect } from "react";
 
-const API_URL = "http://www.omdbapi.com?apikey=1c2bd3a9";
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
+
+const API_URL = "https://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
-	const [searchTerm, setSearchTerm] = useState("");
-	const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
 
-		useEffect(() => {
-			searchMovies("Mad Max");
-		}, []);
+  useEffect(() => {
+    searchMovies("Mad Max");
+  }, []);
 
-	const searchMovies = async (title) => {
-		const response = await fetch(`${API_URL}&s=${title}`);
-		const data = await response.json();
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
-		setMovies(data.Search);
-	};
+    setMovies(data.Search);
+  };
 
-	const handleKeyPress = (e) => {
-		if (e.key === "Enter") {
-			searchMovies(searchTerm);
-		}
-	};
+  const handleKeyPress = (e) => {
+    if(e.key === 'Enter'){
+      searchMovies(searchTerm)
+    }
+  };
 
-	return (
-		<div className="app">
-			<h1>MoviePoster</h1>
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
 
-			<div className="search">
-				<input
-					placeholder="Search for movies"
-					value={searchTerm}
-					onChange={(e) => setSearchTerm(e.target.value)}
-					onKeyPress={handleKeyPress}
-				/>
-				<img
-					src={SearchIcon}
-					alt="search"
-					onClick={() => searchMovies(searchTerm)}
-				/>
-			</div>
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+          onKeyPress={handleKeyPress}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
 
-			{movies?.length > 0 ? (
-				<div className="container">
-					{movies.map((movie) => (
-						<MovieCard movie={movie} />
-					))}
-				</div>
-			) : (
-				<div className="empty">
-					<h2>No movies found</h2>
-				</div>
-			)}
-		</div>
-	);
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.imdbID} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
